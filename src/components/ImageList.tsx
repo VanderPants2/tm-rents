84884398a93279601095af59650517b1d4cd2bfc
Bert Van den Broeck <bert.vandenbroeck@student.odisee.be>
2024-10-29 19:47:36 +0100

    Added image viewer

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import ImageViewer from 'react-simple-image-viewer';
 
 type Props = {
     imageList: {path: string; alt: string}[];
 };
 
 const ImageList = ({imageList}: Props) => {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+    const openImageViewer = useCallback((index) => {
+        setCurrentImage(index);
+        setIsViewerOpen(true);
+    }, []);
+
+    const closeImageViewer = () => {
+        setCurrentImage(0);
+        setIsViewerOpen(false);
+    };
+
     return (
         <div className={'flex justify-center'}>
             <div className={'max-w-[900px]'}>
                 <div className={'w-full text-3xl font-semibold uppercase py-4'}>Foto's</div>
                 <div className={'grid grid-cols-3 gap-6'}>
-                    {imageList.map((image) => (
+                    {imageList.map((image, index) => (
                         <div className={''} key={image.path}>
-                            <img alt={image.alt} className={'aspect-square object-cover rounded-xl'} src={image.path} />
+                            <img
+                                alt={image.alt}
+                                className={'aspect-square object-cover rounded-xl'}
+                                onClick={() => openImageViewer(index)}
+                                src={image.path}
+                            />
                         </div>
                     ))}
+                    {isViewerOpen && (
+                        <ImageViewer
+                            backgroundStyle={{background: 'rgba(0,0,0,0.7)'}}
+                            closeOnClickOutside
+                            currentIndex={currentImage}
+                            disableScroll
+                            onClose={closeImageViewer}
+                            src={imageList.map((image) => image.path)}
+                        />
+                    )}
                 </div>
             </div>
         </div>
--- src/pages/PartyBus.tsx
+++ src/pages/PartyBus.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
 import Header from '../components/Header.tsx';
+import ImageContentBlock from '../components/ImageContentBlock.tsx';
+import ImageList from '../components/ImageList.tsx';
+import Footer from '../components/Footer.tsx';
 
 const PartyBus = () => {
     return (
-        <>
+        <div className={'flex flex-col gap-8'}>
             <Header />
-        </>
+            <ImageContentBlock imageAlt={'partybus'} imagePath={'./img/partybus/12.jpeg'} title={'Partybus'}>
+                <p>
+                    Maak je volgende evenement legendarisch met de Baravan, onze unieke en volledig uitgeruste mobiele tapwagen!
+                    Perfect voor elke gelegenheid, van epische bruiloften en onvergetelijke verjaardagen tot bruisende
+                    bedrijfsfeesten en knallende festivals.
+                </p>
+                <p className={'pt-4'}>Waarom de Baravan?</p>
+                <ul className={'list-disc list-inside'}>
+                    <div className={'indent-3'}>
+                        <li>Professioneel en uniek bar concept 🍻</li>
+                        <li>Volledig uitgerust met tapinstallatie en koeling ❄️</li>
+                        <li>Flexibel en geschikt voor elke locatie 🌍</li>
+                        <li>🎉 Beleef de Plezier!</li>
+                    </div>
+                </ul>
+                <p className={'pt-4'}>
+                    Gasten genieten van perfect getapte drankjes 🍹 Creëer een gezellige sfeer met een stijlvolle barambiance ✨
+                    Maak onvergetelijke herinneringen met vrienden en familie ❤️ Reserveer de Baravan vandaag nog en voeg een
+                    vleugje magie en gemak toe aan je evenement! 🥳🍻
+                </p>
+            </ImageContentBlock>
+            <ImageList
+                imageList={[
+                    {path: './img/partybus/1.jpeg', alt: ''},
+                    {path: './img/partybus/2.jpeg', alt: ''},
+                    {path: './img/partybus/3.jpeg', alt: ''},
+                    {path: './img/partybus/4.jpeg', alt: ''},
+                    {path: './img/partybus/5.jpeg', alt: ''},
+                    {path: './img/partybus/6.jpeg', alt: ''},
+                    {path: './img/partybus/7.jpeg', alt: ''},
+                    {path: './img/partybus/8.jpeg', alt: ''},
+                    {path: './img/partybus/9.jpeg', alt: ''},
+                    {path: './img/partybus/10.jpeg', alt: ''},
+                    {path: './img/partybus/11.jpeg', alt: ''},
+                ]}
+            />
+            <Footer />
+        </div>
     );
 };
 
