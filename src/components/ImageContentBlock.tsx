c4002d7eeffe7f7c608f008428b7771c944680cb
Bert Van den Broeck <bert.vandenbroeck@student.odisee.be>
2024-10-30 21:28:12 +0100

    fix layout

--- src/components/ImageContentBlock.tsx
+++ src/components/ImageContentBlock.tsx
@@ -12,7 +12,7 @@ const ImageContentBlock = ({children, imageAlt, imagePath, reverse, title}: Prop
     return (
         <div className={'flex justify-center px-4'}>
             <div className={'flex items-center max-w-[900px] flex-col sm:flex-row ' + (reverse ? 'sm:flex-row-reverse' : '')}>
-                <div className={'basis-2/3 pb-8 sm:px-8 sm:pb-0'}>
+                <div className={'basis-2/3 pb-8 sm:pr-8 sm:pb-0 ' + (reverse ? 'sm:pr-0 sm:pl-8' : '')}>
                     <div className={'font-bold sm:text-6xl text-4xl uppercase'}>{title}</div>
                     <div>{children}</div>
                 </div>
--- src/components/ListBlock.tsx
+++ src/components/ListBlock.tsx
@@ -10,7 +10,7 @@ const ListBlock = ({children, title}: Props) => {
     return (
         <div className={'flex justify-center py-16 px-4 bg-black text-teal-600'}>
             <div className={'flex items-center max-w-[900px] flex-col sm:flex-row gap-4'}>
-                <div className={'sm:px-8 sm:pb-0 pb-12 px-4 basis-1/3'}>
+                <div className={'sm:pr-8 sm:pb-0 pb-12 basis-1/3'}>
                     <div className={'font-bold sm:text-6xl text-4xl uppercase'}>{title}</div>
                 </div>
                 <ul className={'basis-2/3 flex flex-col gap-4'}>{children}</ul>
